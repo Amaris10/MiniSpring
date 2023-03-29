ce707f566cdee226d21edfb9725c8bf14e6f3b6a
Amaris <amaris@AmarisdeMacBook-Pro.local>
2023-03-29 20:25:40 +0800

    “第一个简单的ioc容器”

---
+++ spring_ioc/src/main/java/com/amaris/test/AServiceImpl.java
@@ -0,0 +1,14 @@
+package com.amaris.test;
+
+import com.amaris.test.AService;
+
+public class AServiceImpl implements AService {
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("a service 1 say hello.");
+
+	}
+
+}
---
+++ spring_ioc/src/test/java/com/amaris/Test1.java
@@ -0,0 +1,17 @@
+package com.amaris;
+
+import com.amaris.test.AService;
+import com.amaris.ClassPathXmlApplicationContext;
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test1(){
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		AService aService = (AService)ctx.getBean("aservice");
+		aService.sayHello();
+	}
+
+
+}
